9c007a8d6fde63bcd8d45d90e8ddbb7ef992510e
Rick Mark <rickmark@outlook.com>
2017-01-02 22:48:19 -0800

    Icon and integrate Estimote SDK

--- HotMess/AppDelegate.swift
+++ HotMess/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let beaconManager = ESTBeaconManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        
         return true
     }
 
---
+++ HotMess/BridgeHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgeHeader.h
+//  HotMess
+//
+//  Created by Rick Mark on 1/2/17.
+//  Copyright © 2017 Hot Mess and Co. All rights reserved.
+//
+
+#import <EstimoteSDK/EstimoteSDK.h>
